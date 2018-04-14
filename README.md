6e67efabf44658624abbdecbcfc79fbd8eef1a38
mario <airvex@utexas.edu>
2018-04-14 14:21:20 -0500

    Fixed time interval problems by converting the time to float seconds

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 roomba_control
 
 Controlling the behaviors of the roomba
+
+HOW TO RUN
+
+Assuming you have catkin, ROS kinetic, Gazebo, etc...
+
+1. Open up a terminal and launch roscore
+2. With roscore running, open another terminal.
+3. Type . ~/catkin_ws/devel/setup.bash (you can probably make Linux do this automatically with the bash file but I'm not sure how to do that...)
+4. Type  roslaunch roomba_control create_world.launch 
+5. The sim should launch now. If it doesn't, please let me know!
+
 # Roomba-Controls
 #Roomba-Controls
--- src/roomba1.py
+++ src/roomba1.py
@@ -8,6 +8,9 @@ import time
 from geometry_msgs.msg import Twist
 
 from math import sin
+ 
+#Global stuff
+msg = Twist()
 
 def main():
 
@@ -15,43 +18,39 @@ def main():
     pub = rospy.Publisher('roomba_control_1/cmd_vel', Twist, queue_size=10)
 
     #intializing the node
-    rospy.init_node('circler', anonymous=True)
+    rospy.init_node('roomba_control', anonymous=True)
 
-    rate = rospy.Rate(2) # 10hz
-    msg = Twist()
+    rate = rospy.Rate(10) 
     v = 0.333
     msg.linear.x = v
     msg.angular.z = 0
-    time = rospy.get_time()
+    time = rospy.get_rostime()
     
-    ##sim gets kind of funky as time goes on...
-    ##
     ##TODO: figure out how units work in rospython and perfect the roomba behavior
-    ##    : figure out how to load multiple roombas...might be done through world file?
-    ##    : fix time issues
+    ##    : fix noise right after 180 rotation
     
     while not rospy.is_shutdown():
 		## 180 degree turn every 20 seconds
-		## TODO: for some reason some intervals are skipped...WHY?
-		if (time > 0 and time % 20 == 0):
-			stop = time + 5
-			while time < stop:
+		if (time.secs > 0 and time.secs % 20 == 0):
+			stop = time.secs + 5.0
+			while time.secs < stop:
 				msg.linear.x = 0
 				msg.angular.z= 1
-				time = rospy.get_time()
+				time = rospy.get_rostime()
 				pub.publish(msg)
 				rate.sleep()
 		## 20 degree noise every 5 seconds
-		elif (time > 0 and time % 5 == 0):
-			t = random.uniform(-1,1)
-			msg.angular.z = t
+		elif (time.secs > 0 and time.secs % 5 == 0):
+			t = random.uniform(-5,5)
+			msg.angular.z = -1
 			msg.linear.x = v
 		## normal trajectory
 		else:
 			msg.angular.z = 0
-			msg.linear.x = v
+			msg.linear.x = v	
 			
-		time = rospy.get_time()
+		time = rospy.get_rostime()
+		rospy.loginfo("Current time %i %i", time.secs, time.nsecs)
 		pub.publish(msg)
 		rate.sleep()
 
