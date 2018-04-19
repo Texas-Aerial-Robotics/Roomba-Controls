108a0931d66b3015844edb29c138505928917ed4
mario <airvex@utexas.edu>
2018-04-18 21:13:21 -0500

    Units drastically improved; 180 turn almost perfect

--- src/roomba1.py
+++ src/roomba1.py
@@ -7,10 +7,13 @@ import time
 
 from geometry_msgs.msg import Twist
 
+
 from math import sin
  
 #Global stuff
 msg = Twist()
+
+
 
 def main():
 
@@ -23,39 +26,47 @@ def main():
     rate = rospy.Rate(10) 
     v = 0.333
     msg.linear.x = v
-    msg.angular.z = 0
+    msg.linear.z = 0
+    current_angle = 0
+
     time = rospy.get_rostime()
     
-    ##TODO: figure out how units work in rospython and perfect the roomba behavior
+    ##TODO: make 180 while loop based on angle
     ##    : fix noise right after 180 rotation
     
     while not rospy.is_shutdown():
 		## 180 degree turn every 20 seconds
 		if (time.secs > 0 and time.secs % 20 == 0):
-			stop = time.secs + 5.0
-			while time.secs < stop:
+			initial_time = time.secs
+			while (current_angle < math.pi):
 				msg.linear.x = 0
-				msg.angular.z= 1
-				time = rospy.get_rostime()
+   				msg.angular.z = math.pi/4
+				flag_180 = 1
+				time = rospy.get_rostime() 
+				current_time = time.secs
+				current_angle = (math.pi/4)*(current_time-initial_time)
 				pub.publish(msg)
-				rate.sleep()
+				rate.sleep()		
 		## 20 degree noise every 5 seconds
-		elif (time.secs > 0 and time.secs % 5 == 0):
-			t = random.uniform(-5,5)
-			msg.angular.z = -1
+		elif (time.secs > 0 and time.secs % 5 == 0 and flag_180 == 0):
+			t = random.uniform(-.349,.349)
+			msg.angular.z = t
 			msg.linear.x = v
 		## normal trajectory
 		else:
 			msg.angular.z = 0
-			msg.linear.x = v	
-			
+			msg.linear.x = v
+
+		flag_180 = 0	
+		current_angle = 0
 		time = rospy.get_rostime()
-		rospy.loginfo("Current time %i %i", time.secs, time.nsecs)
 		pub.publish(msg)
 		rate.sleep()
+
 
 if __name__ == '__main__':
     try:
         main()
     except rospy.ROSInterruptException:
         pass
+
