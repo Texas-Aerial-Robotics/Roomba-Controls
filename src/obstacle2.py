d03cacf5f15f93aa6ad40a07f88a1395db5083f6
mario <airvex@utexas.edu>
2018-04-28 13:05:36 -0500

    Added obstacle roombas. Only collisions are needed now.

---
+++ src/obstacle2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import rospy
+import math
+
+from geometry_msgs.msg import Twist
+from math import sin
+ 
+
+def main():
+
+    #topic differential drive is listening on
+    pub = rospy.Publisher('obstacle_roomba_2/cmd_vel', Twist, queue_size=10)
+
+    #intializing the node
+    rospy.init_node('roomba_control', anonymous=True)
+
+    rate = rospy.Rate(10)
+    msg = Twist() 
+    msg.linear.x = 0
+    msg.angular.z = -.0666
+    
+    while not rospy.is_shutdown():
+	msg.linear.x = .333 
+	pub.publish(msg)
+	rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
+
